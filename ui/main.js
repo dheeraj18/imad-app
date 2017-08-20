cc2f916c2fe8897af62c53721866a52bc1a6078c
dheeraj18 <30974027+dheeraj18@users.noreply.github.com>
2017-08-20 16:46:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,23 @@
 var button = document.getElementById('counter');
-var counter = 0;
-button.onclick=function () {
-    
 
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+button.onclick=function (){
+    
+    var request =new XLMHttpRequest();
+   
+    request.onreadystatechange = function(){
+        if(request.readystate === XLMHttpRequest.DONE)
+        {
+             if(requesy.status===200)
+             {
+               var counter= request.responsetext;
+              var span = document.getElementById('count');
+              span.innerHTML = counter.toString();
+             }
+        }
+        
+    };
+    
+request.open('GET','http://dheerajkakumani.imad.hasura-app.io/counter',true);
+request.send(null);
 };
 
