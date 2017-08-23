b853b1991766f0a2186081270fd6c4f28f1c0c1b
dheeraj18 <30974027+dheeraj18@users.noreply.github.com>
2017-08-23 22:25:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -33,7 +33,8 @@ submit.onclick= function(){
         {
              if(request.status === 200)
              {
-                 var names=['name1','name2','name3','name4'];
+                 var names=request.responsetext;
+                 names=JSON.parse(names);
                 var list='';
                 for(var i=0;i< names.length; i++){
                  list += '<li>' + names[i] +'</li>' ;
@@ -45,7 +46,7 @@ submit.onclick= function(){
         }
     };
     
-request.open('GET','http://dheerajkakumani.imad.hasura-app.io/counter',true);
+request.open('GET','http://dheerajkakumani.imad.hasura-app.io/submit-name?name=' +name,true);
 request.send(null);
         
     
